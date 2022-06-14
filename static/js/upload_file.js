447ae35a8661940c98b4d8f7ce002807a0ced511
MaxChangInnodisk <max_chang@innodisk.com>
2022-06-14 17:27:57 +0800

    feat: rename and add more uploader

--- static/js/upload_file.js
+++ static/js/upload_file.js
@@ -1,8 +1,76 @@
 
-const fileUploader = document.querySelector('[data-target="file-uploader"]');
-fileUploader.addEventListener("change", handleFileUpload);
+// ---------------------------------------------------------------------------------------------------
+const modelFileUploader = document.querySelector('[data-target="model-file-uploader"]');
+modelFileUploader.addEventListener("change", modelFileUpload);
+// ---------------------------------------------------------------------------------------------------
+const labelFileUploader = document.querySelector('[data-target="label-file-uploader"]');
+labelFileUploader.addEventListener("change", labelFileUpload);
+// ---------------------------------------------------------------------------------------------------
+const configFileUploader = document.querySelector('[data-target="config-file-uploader"]');
+configFileUploader.addEventListener("change", configFileUpload);
+// ---------------------------------------------------------------------------------------------------
+const sourceUploader = document.querySelector('[data-target="file-uploader"]');
+sourceUploader.addEventListener("change", sourceFileUpload);
+// ---------------------------------------------------------------------------------------------------
+const editSourceUploader = document.querySelector('[data-target="edit-source-file-uploader"]');
+editSourceUploader.addEventListener("change", editSourceFileUpload);
 
-async function handleFileUpload(e) {
+// ---------------------------------------------------------------------------------------------------
+const importSourceUploader = document.querySelector('[data-target="import-source-file-uploader"]');
+importSourceUploader.addEventListener("change", importSourceFileUpload);
+
+async function importSourceFileUpload(e) {
+  try {
+    const file = e.target.files[0];
+    // setUploading(true);
+    if (!file) return;
+
+    // const beforeUploadCheck = await beforeUpload(file);
+    // if (!beforeUploadCheck.isValid) throw beforeUploadCheck.errorMessages;
+
+    // const arrayBuffer = await getArrayBuffer(file);
+    // const response = await uploadFileAJAX(arrayBuffer);
+    
+    // alert("File Uploaded Success");
+    console.log(file);
+    document.getElementById('import_source_file_label').textContent = file['name'];
+    // showPreviewImage(file);
+  } catch (error) {
+    alert(error);
+    console.log("Catch Error: ", error);
+  } finally {
+    // e.target.value = '';  // reset input file
+    // setUploading(false);
+  }
+}
+
+async function editSourceFileUpload(e) {
+  try {
+    const file = e.target.files[0];
+    // setUploading(true);
+    if (!file) return;
+
+    // const beforeUploadCheck = await beforeUpload(file);
+    // if (!beforeUploadCheck.isValid) throw beforeUploadCheck.errorMessages;
+
+    // const arrayBuffer = await getArrayBuffer(file);
+    // const response = await uploadFileAJAX(arrayBuffer);
+    
+    // alert("File Uploaded Success");
+    console.log(file);
+    document.getElementById('edit_source_file_label').textContent = file['name'];
+    // showPreviewImage(file);
+  } catch (error) {
+    alert(error);
+    console.log("Catch Error: ", error);
+  } finally {
+    // e.target.value = '';  // reset input file
+    // setUploading(false);
+  }
+}
+
+
+async function sourceFileUpload(e) {
   try {
     const file = e.target.files[0];
     // setUploading(true);
@@ -27,7 +95,68 @@ async function handleFileUpload(e) {
   }
 }
 
-// STEP 3: change file object into ArrayBuffer
+async function modelFileUpload(e) {
+  try {
+    const file = e.target.files[0];
+    // setUploading(true);
+    if (!file) return;
+
+    // const beforeUploadCheck = await beforeUpload(file);
+    // if (!beforeUploadCheck.isValid) throw beforeUploadCheck.errorMessages;
+
+    // const arrayBuffer = await getArrayBuffer(file);
+    // const response = await uploadFileAJAX(arrayBuffer);
+    
+    // alert("File Uploaded Success");
+    console.log(file);
+    document.getElementById('import_model_file_label').textContent = file['name'];
+    // showPreviewImage(file);
+  } catch (error) {
+    alert(error);
+    console.log("Catch Error: ", error);
+  } finally {
+    // e.target.value = '';  // reset input file
+    // setUploading(false);
+  }
+}
+// ---------------------------------------------------------------------------------------------------
+async function labelFileUpload(e) {
+  try {
+    const file = e.target.files[0];
+    // setUploading(true);
+    if (!file) return;
+    
+    console.log(file);
+    document.getElementById('import_label_file_label').textContent = file['name'];
+    // showPreviewImage(file);
+  } catch (error) {
+    alert(error);
+    console.log("Catch Error: ", error);
+  } finally {
+    // e.target.value = '';  // reset input file
+    // setUploading(false);
+  }
+}
+// ---------------------------------------------------------------------------------------------------
+async function configFileUpload(e) {
+  try {
+    const file = e.target.files[0];
+    // setUploading(true);
+    if (!file) return;
+    
+    console.log(file);
+    document.getElementById('import_config_file_label').textContent = file['name'];
+    // showPreviewImage(file);
+  } catch (error) {
+    alert(error);
+    console.log("Catch Error: ", error);
+  } finally {
+    // e.target.value = '';  // reset input file
+    // setUploading(false);
+  }
+}
+// ---------------------------------------------------------------------------------------------------
+// change file object into ArrayBuffer
 function getArrayBuffer(fileObj) {
   return new Promise((resolve, reject) => {
     const reader = new FileReader();
@@ -46,8 +175,8 @@ function getArrayBuffer(fileObj) {
     reader.readAsArrayBuffer(fileObj);
   });
 }
-
-// STEP 4: upload file throguth AJAX
+// ---------------------------------------------------------------------------------------------------
+// upload file throguth AJAX
 // - use "new Uint8Array()"" to change ArrayBuffer into TypedArray
 // - TypedArray is not a truely Array,
 //   use "Array.from()" to change it into Array
@@ -73,8 +202,8 @@ function uploadFileAJAX(arrayBuffer) {
     .then(data => data)
     .catch(err => console.log("err", err));
 }
-
-// STEP 5: Create before upload checker if needed
+// ---------------------------------------------------------------------------------------------------
+// Create before upload checker if needed
 function beforeUpload(fileObject) {
   return new Promise(resolve => {
     const validFileTypes = ["image/jpeg", "image/png"];
@@ -96,11 +225,11 @@ function beforeUpload(fileObject) {
     });
   });
 }
-
-// function setUploading(isUploading) {
-//   if (isUploading === true) {
-//     spinner.classList.add("opacity-1");
-//   } else {
-//     spinner.classList.remove("opacity-1");
-//   }
-// }
+// ---------------------------------------------------------------------------------------------------
+function setUploading(isUploading) {
+  if (isUploading === true) {
+    spinner.classList.add("opacity-1");
+  } else {
+    spinner.classList.remove("opacity-1");
+  }
+}
