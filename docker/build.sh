f2862b665d37406c26343df1dfefb3a596efb456
MaxChangInnodisk <max_chang@innodisk.com>
2022-07-18 17:23:22 +0800

    feat: extract vendor zip

--- docker/build.sh
+++ docker/build.sh
@@ -6,7 +6,13 @@ source "${ROOT}/utils.sh"
 IMAGE_NAME="ivit-i-demo"
 printd "Concatenate docker image name: ${IMAGE_NAME}" Cy
 
+# Unpack ZIP file
+if [[ -z `ls static/vendor 2>/dev/null` ]];then
+    printd "Extract vendor library ( vendor.zip )"
+    unzip static/vendor.zip
+fi
+
 # Build the docker image
-cd $ROOT
-printd "Build the docker image. (${IMAGE_NAME})" Cy
+cd ${ROOT};
+printd "Build the docker image. (${IMAGE_NAME})" Cy;
 docker build -t ${IMAGE_NAME} .
