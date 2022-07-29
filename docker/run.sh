b207308ed31067b5fb448ee74013bb91b88590dc
MaxChangInnodisk <max_chang@innodisk.com>
2022-07-29 16:38:41 +0800

    feat: inegrate quick setup in docker/run.sh

--- docker/run.sh
+++ docker/run.sh
@@ -9,38 +9,36 @@ fi
 
 # Define the configuration path
 CONF="ivit-i.json"
-
-# setup parameters
-DOCKER_IMAGE=$(cat ${CONF} | jq -r '.client.docker_image')
-DOCKER_NAME=${DOCKER_IMAGE}
-PORT=$(cat ${CONF} | jq -r '.client.port')
-IP=$(cat ${CONF} | jq -r '.client.ip')
-
+RUN_CMD="./demo.sh"
 WORKSPACE="/workspace"
-SET_VISION=""
-RUN_WEB=true
-RUN_CLI=false
 
-WEB_CMD="./demo.sh"
-CLI_CMD="bash"
-RUN_CMD=${WEB_CMD}
+# Read the config at first time
+IP=$(cat ${CONF} | jq -r '.server.ip')
+PLA=$(cat ${CONF} | jq -r '.server.platform')
+PORT=$(cat ${CONF} | jq -r '.server.port')
 
 # Help information
 function help(){
 	echo "Run the iVIT-I-DEMO environment."
 	echo
-	echo "Syntax: scriptTemplate [-f|wsh]"
+	echo "Syntax: scriptTemplate [-f|b|i|p]"
 	echo "options:"
-	echo "c		run container with CLI mode ( Command Line Interface )"
+	echo "b		brand or platform"   
+    echo "i		ip"
+    echo "p		port"
 	echo "h		help."
 }
 
 # Parse the argument
-while getopts "ch" option; do
+while getopts "b:i:p:" option; do
 	case $option in
-		c )
-			RUN_CLI=true ;;
-		h )
+		b )
+			PLA=$OPTARG ;;
+		i )
+			IP=$OPTARG ;;
+        p )
+			PORT=$OPTARG ;;
+        h )
 			help; exit ;;
 		\? )
 			help; exit ;;
@@ -49,8 +47,19 @@ while getopts "ch" option; do
 	esac
 done
 
-# Run CLI mode
-if [[ ${RUN_CLI} = true ]];then RUN_CMD=${CLI_CMD}; fi
+# Update the parameters of configuration
+CNT=$(jq \
+--arg ip "${IP}" --arg pla "${PLA}" --arg port "${PORT}" \
+'.server.ip = $ip | .server.platform = $pla | .server.port = $port' \
+${CONF})
+
+echo -E "${CNT}" > ${CONF}
+
+# setup parameters
+DOCKER_IMAGE=$(cat ${CONF} | jq -r '.client.docker_image')
+DOCKER_NAME=${DOCKER_IMAGE}
+PORT=$(cat ${CONF} | jq -r '.client.port')
+IP=$(cat ${CONF} | jq -r '.client.ip')
 
 # Allow all device connect to display
 xhost + > /dev/null 2>&1
@@ -62,7 +71,6 @@ DOCKER_CMD="docker run \
 --net=host --ipc=host \
 -w ${WORKSPACE} \
 -v `pwd`:${WORKSPACE} \
-${SET_VISION} \
 ${DOCKER_IMAGE} \"${RUN_CMD}\""
 
 # Show 
