d5e05c947d45edf5225624cc083c8abfd853b17d
MaxChangInnodisk <max_chang@innodisk.com>
2022-08-15 14:03:59 +0800

    chore: normal behavior

--- static/js/entry.js
+++ static/js/entry.js
@@ -1,8 +1,8 @@
 // ==================================================================
 // Init
 const DOMAIN = '172.16.92.130';
-const PORT = '818';
-const PLATFORM = 'nvidia';
+const PORT = '819';
+const PLATFORM = 'intel';
 const SCRIPT_ROOT = `http://${DOMAIN}:${PORT}`;
 
 
--- static/js/stream.js
+++ static/js/stream.js
@@ -1,7 +1,7 @@
 // Basic Information
 const DOMAIN = '172.16.92.130';
-const PORT = '818';
-const PLATFORM = 'nvidia';
+const PORT = '819';
+const PLATFORM = 'intel';
 const SCRIPT_ROOT = `http://${DOMAIN}:${PORT}`;
 
 const IMG_EVENT = "images";
