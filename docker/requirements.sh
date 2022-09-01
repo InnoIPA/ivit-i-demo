40b1a6dbfa2930f5fbf64c34b2fabe3dae487f2c
MaxChangInnodisk <max_chang@innodisk.com>
2022-09-01 11:58:14 +0800

    fix: remove noninterative

--- docker/requirements.sh
+++ docker/requirements.sh
@@ -4,13 +4,10 @@
 ROOT=$(dirname $(realpath $0))
 source "${ROOT}/utils.sh"
 
-# None interactive mode
-HEAD="DEBIAN_FRONTEND=noninteractive"
-
 # Update and Install packages
 apt-get update -q
-${HEAD} apt-get install -qqy boxes tree jq bsdmainutils vim zip
-${HEAD} apt-get install -qqy python3-dev python3-pip
+apt-get install -qqy boxes tree jq bsdmainutils vim zip
+apt-get install -qqy python3-dev python3-pip
 
 # Install python module
 pip3 install -r "${ROOT}/py-requirements.txt"
