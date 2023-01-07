9df43d3b6554197a3f99b1569060f20ff9f6ef78
MaxChangInnodisk <max_chang@innodisk.com>
2023-01-07 18:00:17 +0800

    feat: remove unused code

--- static/js/stream.js
+++ static/js/stream.js
@@ -1,16 +1,13 @@
 // Basic Information
 
-const IMG_EVENT = "images";
-const RES_EVENT = "results";
-
 // Global Variable 
 let uuid = "";
-let gpu = 0;
+let gpu;
 let intervalTime = 5000;
 
 // Global Variable - results event
-let detsList="";
-let gpuInfoCount = 0;
+let socketStream
+const sockEvent = "/results"
 let info = new Array;
 
 // Get uuid from route
@@ -23,8 +20,6 @@ for(let i=0; i < el_path.length; i++){
  
 // Set up the socketio address
 const URL = `http://${DOMAIN}:${PORT}/task/${uuid}/stream`;
-
-let socketStream
 
 function get_detection_data(frameID, dets){
 
@@ -75,54 +70,48 @@ function get_application_data(frameID, dets){
     result_element.scrollTop = result_element.scrollHeight;
 }
 
+function sockMessageEvent(ev){
+    let data = JSON.parse(ev.data);
+    data = JSON.parse(data[uuid])
+    
+    let dets = data["detections"];
+    let frameID = data["idx"];
+    let inferTime = data["inference"];
+    let fps = data["fps"];
+    let liveTime = data["live_time"];
+    // 更新 Information
+    document.getElementById("fps").textContent = fps;
+    document.getElementById("live_time").textContent = `${convertTime(liveTime)}`;
+    
+    // get_detection_data(frameID, dets)
+    get_application_data(frameID, dets)
+}
+
+function sockCloseEvent(){
+    console.log('The connection has been closed successfully.');
+}
+
 $(document).ready(async function(){
     
-    try{
-        socketStream = new WebSocket('ws://' + `${DOMAIN}:${PORT}` + '/results');
-        // socketStream = new WebSocket('ws://' + `${DOMAIN}:${PORT}` + '/results');
+    // Connect Socket
+    try{ 
+        socketStream = new WebSocket('ws://' + `${DOMAIN}:${PORT}` + sockEvent);
+    } catch(e){ 
+        console.warn(e) 
+    }
 
-    } catch(e){ console.warn(e) }
-
-    console.log('...');
     // Update Basic Information
     updateBasicInfo();
 
     // Start the stream
     connectWebRTC(uuid);
 
-    
-    // Update first frame
-    // await getFirstFrame(uuid);
-    // document.getElementById("loader").style.display = "block";
-    // document.getElementById("image").style.display = "none";
-
     // Seting Interval: Update GPU temperature every 5 seconds
-    // window.setInterval(updateGPUTemperature, intervalTime);
+    window.setInterval(updateGPUTemperature, intervalTime);
 
-    
-    socketStream.addEventListener('message', ev => {
-        
-        let data = JSON.parse(ev.data);
-        // console.log(data);
-        data = JSON.parse(data[uuid])
-        
-        let dets = data["detections"];
-        let frameID = data["idx"];
-        let inferTime = data["inference"];
-        let fps = data["fps"];
-        let liveTime = data["live_time"];
-        // 更新 Information
-        document.getElementById("fps").textContent = fps;
-        document.getElementById("live_time").textContent = `${convertTime(liveTime)}`;
-        
-        // get_detection_data(frameID, dets)
-        get_application_data(frameID, dets)
-
-    });
-
-    socketStream.addEventListener('close', ev => {
-        console.log('The connection has been closed successfully.');
-    });
+    // Define Socket Event
+    socketStream.addEventListener('message', sockMessageEvent);
+    socketStream.addEventListener('close', sockCloseEvent);
 
 });
 
@@ -205,170 +194,53 @@ async function setFullScreenEvent(){
     });
 }
 
-async function getFirstFrame(uuid){
-    // Sending data via web api ( /add )
-    $.ajax({
-        url: SCRIPT_ROOT + `/task/${uuid}/get_frame`,
-        contentType: false,
-        type: 'GET',
-        success: function (data, textStatus, xhr) {
-            console.log("Get first frame");
-            const img=document.getElementById('image');
-            img.src="data:image/jpeg;base64,"+data["image"];
-        }
-    })
-}
 
 async function updateBasicInfo(){
 
     let data = await getAPI(`/task/${uuid}/info`);
+    if(!data) return undefined;
 
-    if(!data){
-        return undefined;
+    console.log(data);
+    document.getElementById("title").textContent = data['name'];
+    // document.getElementById("app_name").textContent = data['app_name'];
+    document.getElementById("model").textContent = data['model'];
+    document.getElementById("application").textContent = data['application']["name"];
+    document.getElementById("device").textContent = data['device'];
+    
+    document.getElementById("source").textContent = data['source'];
+    // document.getElementById("status").textContent = data['status'];
+
+    document.getElementById("input_type").textContent = getSourceType(data['source']);
+
+    gpu=data['device'];
+    updateGPUTemperature();
+
+}
+
+async function updateGPUTemperature(){
+   
+    // Get GPU Information
+    const gpuData = await getAPI('/device')
+    if(!gpuData) return undefined;
+
+    // Check GPU is correct
+    if(! gpu in gpuData){
+        console.warn(`Could not find GPU (${gpu}) Information`);
+        return undefined
     }
-    else {
-        console.log(data);
-        document.getElementById("title").textContent = data['name'];
-        // document.getElementById("app_name").textContent = data['app_name'];
-        document.getElementById("model").textContent = data['model'];
-        document.getElementById("application").textContent = data['application']["name"];
-        document.getElementById("device").textContent = data['device'];
-        
-        document.getElementById("source").textContent = data['source'];
-        // document.getElementById("status").textContent = data['status'];
 
-        document.getElementById("input_type").textContent = getSourceType(data['source']);
+    // Get Target Info
+    let temperature = gpuData[gpu]["temperature"];
+    // let memoryUtil = gpuData[gpu]["memoryUtil"];
+    
+    // Keep data length is 6
+    if (myLineChart.data.labels.length>=6) rmFirstData(myLineChart);
 
-        gpu=data['device'];
-        updateGPUTemperature();
-    };
-}
+    // Define the chart parameters
+    let today = new Date();
+    let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
 
-function updateBasicInfoLegacy(){
-
-    $.ajax({
-        type: 'GET',
-        url: SCRIPT_ROOT + '/device',
-        dataType: "json",
-        success: function (gpuData){
-            // Get the default
-            console.log("Get device")
-            $.ajax({  
-                type: 'GET',
-                url: SCRIPT_ROOT + `/task/${uuid}/info`,
-                dataType: "json",
-                success: function (data, textStatus, xhr) {
-                    // const data = JSON.parse(data);
-                    console.log(data);
-                    document.getElementById("title").textContent = data['name'];
-                    // document.getElementById("app_name").textContent = data['app_name'];
-                    document.getElementById("model").textContent = data['model'];
-                    document.getElementById("application").textContent = data['application']["name"];
-                    document.getElementById("device").textContent = data['device'];
-                    
-                    document.getElementById("source").textContent = data['source'];
-                    document.getElementById("status").textContent = data['status'];
-
-                    document.getElementById("input_type").textContent = getSourceType(data['source']);
-
-                    gpu=data['device'];
-                    // for(let i=0;i<gpuData.length;i++){
-                    //     console.log(i)
-                    //     if (gpuData[i]['name']==data['device']){
-                    //         gpu=i;
-                    //     };
-                    // };
-                    updateGPUTemperature();
-                },
-                // error: function (xhr, textStatus, errorThrown) {
-                //     alert('err');
-                // }
-            });
-        },
-    });
-}
-
-function updateGPUTemperature(){
-   // 更新 GPU 的溫度
-    $.ajax({
-        type: 'GET',
-        url: SCRIPT_ROOT + '/device',
-        dataType: "json",
-        success: function (data){
-
-            if (gpu !== null){
-                var today = new Date();
-                var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-                var temperature = data[gpu]["temperature"];
-                var memoryUtil = data[gpu]["memoryUtil"];
-                
-                // 當資訊大於 5 的時候也刪除第一筆資料
-                if (myLineChart.data.labels.length>5){
-                    rmFirstData(myLineChart);
-                };
-                addData(myLineChart, time, temperature);
-            } else{
-                for(let i=0; i<data.length; i++){
-                    if (document.getElementById("device").textContent===data[i]['name']){
-                        console.log('Got gpu index');
-                        gpu=i;
-                    }
-                };
-            };
-        }
-    })
+    // Add Data and draw
+    addData(myLineChart, time, temperature);
+    
 };
-
-// const streamSocket = io.connect(URL);
-// let firstFrame = true
-// streamSocket.on(IMG_EVENT, function(msg){  
-    
-//     // if (firstFrame==true){
-//     //     document.getElementById("loader").style.display = "none";
-//     //     document.getElementById("image").removeAttribute("style");
-//     // }
-//     firstFrame = false
-//     const img=document.getElementById('image');
-//     img.src="data:image/jpeg;base64,"+msg;
-//     document.querySelector('#fullpage').style.backgroundImage = 'url(' + img.src + ')';
-// });
-
-// streamSocket.on(RES_EVENT, function(msg){
-
-//     // 解析資料
-//     const data = JSON.parse(msg);
-//     let dets = data["detections"];
-//     let frameID = data["idx"];
-//     let inferTime = data["inference"];
-//     let fps = data["fps"];
-//     let liveTime = data["live_time"];
-    
-//     // 更新 Information
-//     document.getElementById("fps").textContent = fps;
-//     document.getElementById("live_time").textContent = `${convertTime(liveTime)}`;
-    
-//     // 更新 LOG
-//     const result_element=document.getElementById('result');
-
-//     detsList += `<p> Frame ID: ${frameID} </p>`;
-//     if (Array.isArray(dets)) {
-
-//         for(let i=0; i<dets.length; i++){
-//             detsList += `[ ${i} ] \t`;
-            
-//             let detail = Object.keys(dets[i]);
-//             detail.forEach(function(key, index){
-//                 detsList += `${key}: ${dets[i][key]} \t`;
-//             })
-//             detsList += "</p>";
-//         }
-//         detsList += "<hr>";
-//     }
-//     info.push(detsList);
-//     if(info.length>10){ 
-//         detsList = "";
-//         info.shift(); 
-//     }
-//     result_element.innerHTML = info;
-//     result_element.scrollTop = result_element.scrollHeight;
-// });
