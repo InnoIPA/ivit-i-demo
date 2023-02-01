113b96194227f7afc1f32186d26045a8358a9517
MaxChangInnodisk <max_chang@innodisk.com>
2023-01-31 19:30:23 -0800

    chore: normal behavior

--- static/js/common.js
+++ static/js/common.js
@@ -1,5 +1,5 @@
-const DOMAIN = '172.16.92.130';
-const PORT = '819';
+const DOMAIN = '10.204.16.118';
+const PORT = '820';
 
 const SCRIPT_ROOT       = `http://${DOMAIN}:${PORT}`;
 const DEBUG_MODE        = false;
